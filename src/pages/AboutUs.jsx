2a652b296c9838a970072ac3c3be94bc03e7e3cd
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2024-10-12 23:30:57 -0500

    Finished adding basic structure of site -Jet

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are committed to helping animals find loving homes.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/pages/ViewAllAdoptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ViewAllAdoptions = () => {
+  return (
+    <div>
+      <h1>All Adoptions</h1>
+      <p>List of available animals for adoption will be displayed here.</p>
+    </div>
+  );
+};
+
+export default ViewAllAdoptions;
