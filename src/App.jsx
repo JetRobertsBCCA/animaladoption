a1171efec46dca92c72f02f06050120bf70203a2
JetRobertsBCCA <jroberts@basecampcodingacademy.org>
2024-10-21 12:52:28 -0500

    GH-PAGES edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import AddAnimalForm from "./components/AddAnimalForm";
 import AnimalListings from "./components/AnimalListings";
 import DeleteAllRecords from "./components/DeleteAllRecords";
